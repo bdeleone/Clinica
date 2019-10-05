53dccb88baf57d8f47ea23b7845305d16edcaad2
bdeleone <56199118+bdeleone@users.noreply.github.com>
2019-10-05 10:52:42 -0600

    update

--- src/regdoctor/doc.java
+++ src/regdoctor/doc.java
@@ -10,5 +10,40 @@ package regdoctor;
  * @author umg
  */
 public class doc {
-    
+    private String nombred;
+    private String edadd;
+    private String colegiadod;
+    private String especialidadd;
+
+    public String getNombred() {
+        return nombred;
+    }
+
+    public void setNombred(String nombred) {
+        this.nombred = nombred;
+    }
+
+    public String getEdadd() {
+        return edadd;
+    }
+
+    public void setEdadd(String edadd) {
+        this.edadd = edadd;
+    }
+
+    public String getColegiadod() {
+        return colegiadod;
+    }
+
+    public void setColegiadod(String colegiadod) {
+        this.colegiadod = colegiadod;
+    }
+
+    public String getEspecialidadd() {
+        return especialidadd;
+    }
+
+    public void setEspecialidadd(String especialidadd) {
+        this.especialidadd = especialidadd;
+    }
 }
