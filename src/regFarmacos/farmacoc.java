7ca23377d5b5a4a96326ae20e68bcf8185b22315
oliverluciani <56199150+oliverluciani@users.noreply.github.com>
2019-10-05 10:22:30 -0600

    ingreso de farmacos clases y pantallas

--- src/regFarmacos/farmacoc.java
+++ src/regFarmacos/farmacoc.java
@@ -10,5 +10,52 @@ package regFarmacos;
  * @author umg
  */
 public class farmacoc {
+    public String nombre;
+    public String descripcion;
+    public String valorcompra;
+    public String valorventa;
+    public String proveedor;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getValorcompra() {
+        return valorcompra;
+    }
+
+    public void setValorcompra(String valorcompra) {
+        this.valorcompra = valorcompra;
+    }
+
+    public String getValorventa() {
+        return valorventa;
+    }
+
+    public void setValorventa(String valorventa) {
+        this.valorventa = valorventa;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+    
+    
     
 }
